a10761adb51119985526cc5c4030697e62eecd3a
Ticklish Elmo <99320974+TaquiRaza@users.noreply.github.com>
2026-05-17 18:51:59 -0700

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+
+        answer = []
+        curr = []
+
+        def backtrack(i, curr, answer):
+            total = sum(curr)
+            if total == target:
+                answer.append(curr.copy())
+                return True
+
+            if total > target:
+                return False
+
+            if i == len(nums):
+                return False
+            
+            # try repeating the same number
+            curr.append(nums[i])
+            backtrack(i, curr, answer)
+            curr.pop()
+
+            # try skipping the current number
+            backtrack(i+1, curr, answer)
+        
+        backtrack(0, curr, answer)
+
+        return answer
